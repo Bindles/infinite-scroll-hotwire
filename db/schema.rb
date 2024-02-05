c02ea9ad7035a8a2adf185297d088a9d514e58e8
Bindles <bindlez@gmail.com>
2024-02-04 19:59:38 -0500

    fecth_by_param.to_i

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_29_221815) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_03_231451) do
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.text "body"
@@ -24,6 +24,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_29_221815) do
     t.string "name"
     t.string "category"
     t.text "desc"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pokemons", force: :cascade do |t|
+    t.string "name"
+    t.string "url"
+    t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
